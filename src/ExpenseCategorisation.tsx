26190489000294a59210713c2f3734fbee618143
EmmanuellaOsikoya <G00420464@atu.ie>
2025-11-10 14:25:10 +0000

    Created ExpenseCategorisation.tsx

---
+++ src/ExpenseCategorisation.tsx
@@ -0,0 +1,5 @@
+// This file is responsible for categorising expenses based on user input
+// The user will drag and drop a bank statement file here and tensorflows BERT model will be used to categorise each expense
+
+// imports needed for this file
+import React, { useState, DragEvent, ChangeEvent} from 'react';
