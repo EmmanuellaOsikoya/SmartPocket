df88702d6da62e968e8f5af1862e3ff6aaafdbe5
EmmanuellaOsikoya <G00420464@atu.ie>
2025-11-11 13:27:20 +0000

    Home Page now links to Expense Categorisation Page

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,6 +4,10 @@ const Home: React.FC = () => {
     return (
         <div className="p-10">
             <h1 className="text-3xl font-bold mb-6">Welcome to SmartPocket</h1>
+            <h2 className="text-2xl font-semibold mb-4">What would you like to do?</h2>
+
+            <a href="/expense-categorisation" className="text-blue-500 hover:underline">Put my expenditure into categories</a>
+            
         </div>
     );
 };
