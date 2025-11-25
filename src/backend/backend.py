bf356a19d83f91c3455a1f4559eae8ed6a142f0e
EmmanuellaOsikoya <G00420464@atu.ie>
2025-11-25 13:49:20 +0000

    Created function that categorises expenses

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -15,3 +15,29 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Keyword-based rule system for expense categorisation
+CATEGORIES = {
+    "Groceries": ["Tesco", "Aldi", "Lidl", "Dunnes", "Dealz", "Grocery", "Supermarket"],
+    "Rent/Ultilites": ["Rent", "Electric", "Gas", "Water", "Eir", "Wifi", "Internet"],
+    "Transport": ["Burkes Bus", "Trnasport for Ireland", "Irish Rail"],
+    "Healthcare": ["Pharmacy", "Doctor", "Hospital", "Clinic"],
+    "Food": ["Restaraunt", "Just Eat", "SuperMacs", "McDonalds"],
+    "Subscriptions": ["Spotify", "Netflix", "Cinema"]
+    
+}
+
+# Function that catgeorises a single transaction's description using rules
+def categorise_description(text: str):
+    text = text.lower()
+
+    # Loop through each category
+    for category, keywords in CATEGORIES.items():
+        # Check if any keyword appears in the text
+        if any(k in text for k in keywords):
+            return category
+    
+    # Default category if nothing matched
+    return "Other"
+
+
