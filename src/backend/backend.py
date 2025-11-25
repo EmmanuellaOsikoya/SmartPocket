5c9b1d1a76695d2fc97e782e8a5ca899d3419d2a
EmmanuellaOsikoya <G00420464@atu.ie>
2025-11-25 13:38:11 +0000

    Requests from frontend now allowed

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -4,3 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 import pdfplumber
 import re
 
+# Initialises FastAPI application
+app = FastAPI()
+
+# Allows requests from React frontend 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React dev server
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
